87ada8f1f202cbfe90bafa3fe321a4c72efc4ed8
code-with-shakti <shaktiprasadswain2004@gmail.com>
2025-09-21 23:46:04 +0530

    Initial commit of Siri assistant project with requirements.txt

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+# musicLibrary.py
+
+# Dictionary of songs and their YouTube links
+music = {
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "despacito": "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"
+}
